1bc0652d1f08c96177f734efe43534186e9290aa
Rafael Souza <rafael.ssouza@gmail.com>
2008-09-18 21:23:47 -0300

    added markup to create wiki pages easily

--- siniki.rb
+++ siniki.rb
@@ -6,6 +6,21 @@ require 'rdiscount'
 
 DataMapper.setup(:default, 'sqlite3:siniki.db')
 
+module Siniki
+  module Markdown
+
+    def new(html)
+      super(html.gsub(/\[\[([\w |]+)\]\]/) do |m|
+        title, link = $1.split('|')
+
+        "[#{title}](/#{(link||title).to_permalink})"
+      end)
+    end
+  end
+end
+
+RDiscount.extend(Siniki::Markdown)
+
 class String
   def to_permalink
     str = Unicode.normalize_KD(self).gsub(/[^\x00-\x7F]/n,'')
