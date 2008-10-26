1fb62401baf741413e9f6d48cd9c38ddb4fc0980
Rafael Souza <rafael.ssouza@gmail.com>
2008-10-26 20:19:01 -0200

    some modifications

--- siniki.rb
+++ siniki.rb
@@ -1,23 +1,21 @@
 #!/usr/bin/env ruby
-# -*- coding: utf-8 -*-
 
 require 'rubygems'
 require 'dm-core'
 require 'sinatra'
 require 'unicode'
 require 'rdiscount'
+require 'actions'
+require 'models'
+require 'helpers'
 
 enable :sessions
 
-DataMapper.setup(:default, 'sqlite3:siniki.db')
-
 module Siniki
   module Markdown
-
     def new(html)
       super(html.gsub(/\[\[([\w |]+)\]\]/) do |m|
         title, link = $1.split('|')
-
         "[#{title}](/#{(link||title).to_permalink})"
       end)
     end
@@ -34,62 +32,15 @@ class String
   end
 end
 
-class Page
-  include DataMapper::Resource
-  property :id, Integer, :serial => true
-  property :title, String, :nullable => false
-  property :body, Text
-  property :processed_body, Text
-  property :permalink, String, :nullable => false
-  property :created_at, DateTime
-  property :updated_at, DateTime
-
-  before :save, :set_permalink
-  before :save, :process_body
-
-  def self.welcome
-    Page.first(:permalink => 'welcome')
-  end
-
-  def self.header
-    Page.first(:permalink => 'header')
-  end
-
-  def self.menu
-    Page.first(:permalink => 'menu')
-  end
-
-  private
-
-  def set_permalink(context = :default)
-    self.permalink = self.title.to_permalink if self.permalink.nil?
-  end
-
-  def process_body
-    unless self.body.nil?
-      markdown = RDiscount.new(self.body)
-      self.processed_body = markdown.to_html
-    end
-  end
-end
-
-class User
-  include DataMapper::Resource
-  property :id, Integer, :serial => true
-  property :username, String, :nullable => false
-  property :password, String, :nullable => false
-
-  def self.login(username, password)
-    first(:username => username, :password => password)
-  end
-end
 
 before do
-  @header = Page.header.processed_body
-  @menu = Page.menu.processed_body
+  @header = Page.header.html_body
+  @menu = Page.menu.html_body
 end
 
 get '/setup' do
+  require_login
+
   DataMapper.auto_migrate!
 
   page = Page.new
@@ -109,44 +60,23 @@ get '/setup' do
   user.attributes = {:username => 'admin', :password => 'aaa123' }
   user.save
 
-  "siniki is ready to run!"
+  "<p>siniki is ready to run!</p><a href='/'>Go to home</a>"
 end
-
-helpers do
-  def logged_in?
-    session[:username]
-  end
-
-  def require_login
-    unless logged_in?
-      session[:redirect_back_to] = request.env['REQUEST_URI']
-      redirect '/login'
-    end
-  end
-end
-
-get '/' do
-  redirect '/welcome'
-end
-
-#get '/edit' do
-#  page = Page.welcome
-#  "<form action='/save' method='post'><input type='hidden' name='id' value='#{page.id}'/><h2>Edit welcome page</h2><label>Body</label><br/><textarea name='body' rows='28' cols='100'>#{page.body}</textarea><br/><input type='submit' value='Salvar'/></form>"
-#end
 
 post '/save' do
   if params[:title].nil?
     params[:title] = 'Welcome'
   end
 
-  if params[:id].to_i.nonzero?
-    page = Page.get(params[:id])
-  else
-    page = Page.new
-    #params[:permalink] = params[:title].to_permalink unless params[:title].nil?
+  page_id = params.delete('id')
+
+  page = Page.new
+  page.attributes = params
+
+  if page_id.to_i.nonzero?
+    page.page = Page.get(page_id)
   end
 
-  page.attributes = params
   if page.save
     redirect "/#{page.permalink}"
   else
@@ -179,11 +109,11 @@ get '/logout' do
 end
 
 get '/:permalink' do
-  @page = Page.first(:permalink => params[:permalink])
+  @page = Page.last_version(params[:permalink])
   if @page
     haml :page
   else
-    redirect '/' + params[:permalink] + '/new'
+    redirect "/#{params[:permalink]}/new"
   end
 end
 
@@ -197,6 +127,10 @@ end
 get '/:permalink/edit' do
   require_login
 
-  @page = Page.first(:permalink => params[:permalink])
+  @page = Page.last_version(params[:permalink])
   haml :edit
+end
+
+get '/' do
+  redirect '/welcome'
 end
